d68b13c589d33c61dcf33bec44a33495d7f44d44
Rodrigo <rorrito63@gmail.com>
2026-09-02 21:09:31 -0400

    commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@u3-+vlkp%(o9vf79i(8a9%24#y8k6%3v--(1%o5_hn8c$xk+o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
